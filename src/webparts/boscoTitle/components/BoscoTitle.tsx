c1b2b9d1240a0fad6c5cb1d50333db3b69e3f7fb
tg322 <tom.shaun.grennan@gmail.com>
2023-07-03 16:00:34 +0100

    Pnpified version of BoscoTitle

--- src/webparts/boscoTitle/components/BoscoTitle.tsx
+++ src/webparts/boscoTitle/components/BoscoTitle.tsx
@@ -6,23 +6,25 @@ import { IBoscoTitleProps } from './IBoscoTitleProps';
 function BoscoTitle(props: IBoscoTitleProps){
   const {
     image1,
-    image1Position,
-    image2,
-    image2Position,
-    context
+    image1Position
   } = props;
-  console.log(context);
   return(
     <section className={`${styles.test}`}>
         
         <div id={image1 && image1.label ? image1.label : ''} style={{backgroundImage: `url(${image1 && image1.blob ? image1.blob : ''})`, backgroundSize:'cover', backgroundPosition:image1Position}}>
-        
+
+          <div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
 
         </div>
 
-        <div id={image2 && image2.label ? image2.label : ''} style={{backgroundImage: `url(${image2 && image2.blob ? image2.blob : ''})`, backgroundSize:'cover', backgroundPosition:image2Position}}>
+        {/* <div id={image2 && image2.label ? image2.label : ''} style={{backgroundImage: `url(${image2 && image2.blob ? image2.blob : ''})`, backgroundSize:'cover', backgroundPosition:image2Position}}>
           
-        </div>
+        </div> */}
         
       </section>
   );
